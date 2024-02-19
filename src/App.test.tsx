7eedaaa38f73b630910cea5db951745bdfef27b0
Ulugbek's macbook air m2 <ulugb3k270@gmail.com>
2024-02-20 02:41:41 +0500

    feat: implemented reusable modal

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ModalProvider } from "./context/modal-context/modal.context";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders Header component", () => {
+    render(
+      <BrowserRouter>
+        <ModalProvider>
+          <App />
+        </ModalProvider>
+      </BrowserRouter>,
+    );
+    const headerElement = screen.getByTestId("header-component");
+    expect(headerElement).toBeInTheDocument();
+  });
 });
