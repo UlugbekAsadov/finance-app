2215d36635dd69507903719e4c042a1831e9ecd5
Ulugbek's macbook air m2 <ulugb3k270@gmail.com>
2024-02-20 02:41:04 +0500

    feat: implemented reusable modal

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./assets/styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { ModalProvider } from "./context/modal-context/modal.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ModalProvider>
+        <App />
+      </ModalProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
