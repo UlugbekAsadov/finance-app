2daef68a01b4ddb776c95cfd7a7e813504a71f7f
Ulugbek's macbook air m2 <ulugb3k270@gmail.com>
2024-02-20 14:57:48 +0500

    feat: implemented transaction test

---
+++ src/components/transactions/transactions.test.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import { Transactions } from "./transactions";
+import { useApi } from "../../hooks/useApi/useApi";
+import { ITransactionResponse } from "../../utils/interfaces/transaction-actions.interface";
+import { ETransactionActions } from "../../utils/enums/transaction-actions.enum";
+
+jest.mock("../../hooks/useApi/useApi", () => ({
+  useApi: jest.fn(),
+}));
+
+describe("Transactions Component", () => {
+  test("renders transactions list when data is available", async () => {
+    const mockTransactions: ITransactionResponse[] = [
+      {
+        id: 1,
+        title: "Salary",
+        timestamp: 1708419643728,
+        price: 2000,
+        type: ETransactionActions.Income,
+      },
+      {
+        id: 2,
+        title: "Groceries",
+        timestamp: 1708419643728,
+        price: 50,
+        type: ETransactionActions.Outcome,
+      },
+    ];
+
+    (useApi as jest.Mock).mockReturnValue({ data: mockTransactions });
+
+    const { getByText } = render(<Transactions />);
+
+    await waitFor(() => {
+      const salaryElement = getByText("Salary");
+      const groceriesElement = getByText("Groceries");
+
+      expect(salaryElement).toBeInTheDocument();
+      expect(groceriesElement).toBeInTheDocument();
+    });
+  });
+
+  test("does not render transactions list when data is not available", () => {
+    (useApi as jest.Mock).mockReturnValue({ data: [] });
+
+    const { container } = render(<Transactions />);
+
+    expect(container.firstChild).toBeNull();
+  });
+});
